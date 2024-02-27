3feec929d519a4f5d828d90f689b1c2123b4ffc9
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2024-02-27 12:14:02 +0100

    i added initia swagge ui

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+  /**
+     * @OA\Info(
+     *    title="Taskify Api",
+     *    version="1.0.0",
+     * )
+     * @OA\SecurityScheme(
+     *     type="http",
+     *     securityScheme="bearerAuth",
+     *     scheme="bearer",
+     * )
+
+     */
+
+
 class Controller extends BaseController
 {
+
     use AuthorizesRequests, ValidatesRequests;
 }
